7103041a26583a224da57dbaf96e01035888a44d
Afzal <14029371+afzalsayed96@users.noreply.github.com>
2019-03-13 16:20:50 +0530

    adds quantized model

--- convert.py
+++ convert.py
@@ -1,5 +1,5 @@
 import tensorflowjs as tfjs
 import keras
-
+import numpy as np
 model = keras.models.load_model('./models/pets-nasnetmobile-all-nontrainable-30-0.91.hdf5')
-tfjs.converters.save_keras_model(model, "./models/pets")
+tfjs.converters.save_keras_model(model, "./models/quantized", quantization_dtype=np.uint8)
